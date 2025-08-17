95b69918928bf7b4a0fadaf2e4ed4da66d2b8903
Methuseli <mfemameckson@gmail.com>
2025-08-17 22:18:12 +0200

    updated docker config for frontend

--- client/app/config.ts
+++ client/app/config.ts
@@ -1,2 +1,2 @@
-export const baseUrl = "https://18.222.104.72.nip.io/client/api/v1/";
-export const bankingUrl = "https://18.222.104.72.nip.io/banking/api/v1/";
+export const baseUrl = "http://client-service/api/v1/";
+export const bankingUrl = "http://banking-service/api/v1/";
