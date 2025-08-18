f5cda40d290386c76dcb820cc86022be70f4b539
Methuseli <mfemameckson@gmail.com>
2025-08-18 13:47:59 +0200

    swagger annotation

--- services/client/src/main/java/com/microbank/client/ClientApplication.java
+++ services/client/src/main/java/com/microbank/client/ClientApplication.java
@@ -3,7 +3,10 @@ package com.microbank.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
+@OpenAPIDefinition
 public class ClientApplication {
 
 	public static void main(String[] args) {
