a17e7884bea622510cfaa501ae2a11507248195c
Methuseli <mfemameckson@gmail.com>
2025-08-19 21:51:20 +0200

    revert backend url

--- client/app/config.ts
+++ client/app/config.ts
@@ -1,5 +1,5 @@
-// export const baseUrl = "http://13.59.11.243.sslip.io/client/api/v1/";
-// export const bankingUrl = "http://13.59.11.243.sslip.io/banking/api/v1/";
+export const baseUrl = "http://13.59.11.243.sslip.io/client/api/v1/";
+export const bankingUrl = "http://13.59.11.243.sslip.io/banking/api/v1/";
 
-export const baseUrl = "http://localhost:8081/client/api/v1/";
-export const bankingUrl = "http://localhost:8080/banking/api/v1/";
+// export const baseUrl = "http://localhost:8081/client/api/v1/";
+// export const bankingUrl = "http://localhost:8080/banking/api/v1/";
